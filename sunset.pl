955db09cfc2309f107e935d5791339851751bdd3
Karl <znarlie@gmail.com>
2025-05-11 09:42:25 +0800

    Changed username and password handling and changed nighttime var handling

--- sunset.pl
+++ sunset.pl
@@ -5,6 +5,7 @@ use DateTime::Event::Sunrise;
 use Math::Round;
 use Net::Ping;
 use IO::Handle;
+use Env qw($TAPO_USERNAME $TAPO_PASSWORD);
 use strict;
 
 
@@ -18,8 +19,8 @@ my $ps5_power_ip = "192.168.100.6";
 my $longitude = 101;
 my $latitude = 3;
 
-my $brightness = 100;
-my $daytime_brightness_mod = 99;
+my $brightness = "";
+my $daytime_brightness_mod = 49;
 my $in_room_mod = 50;
 my $hue = 0;
 my $saturation = 100;
@@ -33,17 +34,6 @@ my $set_lamp_office = "";
 my $set_lamp_livingroom = "";
 
 
-my ($tapo_username, $tapo_password) = @ARGV;
-
-# Check if username and password are provided
-if (!defined $tapo_username || !defined $tapo_password) {
-    die "Usage: $0 <username> <password>\n";
-}
-
-# Sanitize username and password
-$tapo_username =~ s/^\s+|\s+$//g;  # Trim leading and trailing whitespace
-$tapo_password =~ s/^\s+|\s+$//g;  # Trim leading and trailing whitespace
-
 my $tty;
 sub isatty() { 
   no autodie; 
@@ -51,7 +41,7 @@ sub isatty() {
 }
 
 isatty();
-print "Is someone watching?  There is someone! Gotta explain what we are doing as we do it for the humans.\n" if ( $tty );
+print "Found a TTY, printing debug.\n" if ( $tty );
 
 my $dt = DateTime->now;             
 my $sunrise = DateTime::Event::Sunrise ->sunrise ( longitude =>$longitude, latitude  =>$latitude, );
@@ -63,7 +53,6 @@ my $day_set = DateTime::SpanSet->from_sets(
   start_set => $sunrise, end_set => $sunset );
 my $is_it_day = $day_set->contains( $dt ) ? '1' : '0';
 
-print "Work out what time of day it is and what colour we need to set.\n" if ( $tty );
 # 0 - 6
 if (( $hour >= 0 ) and( $hour < 6 )) {
   $hue = round (( $minute_of_day / 360 ) * 260 );  # between 0 and 260 
@@ -81,12 +70,15 @@ if (( $hour >= 0 ) and( $hour < 6 )) {
   $saturation = 100;
 }
 
-$brightness = 100;
+$brightness = 50;
 print "Make the light a bit brighter it is nighttime.\n" if ( $tty );
 if ( $is_it_day ) { $brightness = $brightness - $daytime_brightness_mod; }
 
-$wimpy_power = get_power_usage ($wimpy_power_ip, $tapo_username, $tapo_password);
-$ps5_power = get_power_usage ($ps5_power_ip, $tapo_username, $tapo_password);
+$wimpy_power = get_power_usage ($wimpy_power_ip, $TAPO_USERNAME, $TAPO_PASSWORD);
+$ps5_power = get_power_usage ($ps5_power_ip, $TAPO_USERNAME, $TAPO_PASSWORD);
+
+
+print "Wimpy current power usage is $wimpy_power W > $wimpy_power_inroom_trigger W threshold for in use and brightness is $brightness\n" if ( $tty );
 
 
 if ( $wimpy_power > $wimpy_power_inroom_trigger )  { 
@@ -100,13 +92,13 @@ if ( $tty ) {
 	print "Wimpy Power is $wimpy_power W and daytime is $is_it_day, PS5 Power is $ps5_power W. Is it day? $is_it_day \n";
 };
 
-set_lamp_colour ($bulb_ip_office, $tapo_username, $tapo_password, $hue, $saturation, $brightness);
+set_lamp_colour ($bulb_ip_office, $TAPO_USERNAME, $TAPO_PASSWORD, $hue, $saturation, $brightness);
 
-$brightness = 100;
+$brightness = 50;
 print "Make the light a bit brighter it is nighttime.\n" if ( $tty );
 if ( $is_it_day ) { $brightness = $brightness - $daytime_brightness_mod; }
 
-print "$ps5_power > $ps5_power_inroom_trigger $brightness\n" if ( $tty );
+print "PS5 $ps5_power W >  $ps5_power_inroom_trigger threshold and brightness is $brightness\n" if ( $tty );
 
 if ( $ps5_power > $ps5_power_inroom_trigger )  { 
   $brightness = $brightness + $in_room_mod; 
@@ -115,7 +107,7 @@ if ( $ps5_power > $ps5_power_inroom_trigger )  {
 if ( $brightness > 100 ) { $brightness = 100; }
 if ( $brightness < 1 ) { $brightness = 1; }
 
-set_lamp_colour ($bulb_ip_livingroom, $tapo_username, $tapo_password, $hue, $saturation, $brightness);
+set_lamp_colour ($bulb_ip_livingroom, $TAPO_USERNAME, $TAPO_PASSWORD, $hue, $saturation, $brightness);
 
 sub get_power_usage {
     my ($device_ip, $username, $password) = @_;
@@ -143,7 +135,7 @@ sub set_lamp_colour {
     my ($device_ip, $username, $password, $hue, $saturation, $brightness) = @_;
 
     # Run the kasa command to set the lamp color
-    print "Hue, Saturation and Brightness values for lamp set to $hue $saturation $brightness\n";
+    print "$device_ip Hue, Saturation and Brightness is $hue $saturation $brightness\n" if ( $tty );
     my $command_output = `/usr/local/bin/kasa --host $device_ip --username $username --password $password hsv $hue $saturation $brightness`;
 
     # Check if the command was successful
